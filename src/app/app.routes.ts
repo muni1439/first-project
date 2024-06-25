5e3c19d338d9ddcdc0997bb2979c8f0ca9837e3d
muni1439 <kapisetti.syr2022@gmail.com>
2024-06-25 14:50:39 -0400

    updated some files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'portfolio-website', pathMatch: 'full' },
+    { path: '', redirectTo: 'portfolio-website/home', pathMatch: 'full' },
     {path:'portfolio-website' , loadChildren: () => import('./portfolio-website/portfolio-website.module').then(m => m.PortfolioWebsiteModule)}
 ];
+
+providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}]
