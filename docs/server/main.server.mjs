2864dacef8b2521e13f384ba1be6e32e5e9b03d4
muni1439 <kapisetti.syr2022@gmail.com>
2024-06-25 16:11:52 -0400

    Initial commit

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-CY73N2F4.mjs";import"./chunk-FM2EPCM7.mjs";import"./chunk-RODZR77S.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,6 @@
 import { Routes } from '@angular/router';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'portfolio-website/home', pathMatch: 'full' },
     {path:'portfolio-website' , loadChildren: () => import('./portfolio-website/portfolio-website.module').then(m => m.PortfolioWebsiteModule)}
 ];
-
-providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}]
